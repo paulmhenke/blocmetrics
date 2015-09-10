4cc6b31c19dae190def3a7617965bf84e6ad4b69
paulhenke <paulmhenke@gmail.com>
2015-09-10 23:50:03 +0000

    applications_controller and basic application#index view

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,11 +2,30 @@ class ApplicationsController < ApplicationController
     
   def index
     @user = current_user
-    @user_applications = @user.applications
+    @applications = @user.applications
   end
   
   def new
     @user = current_user
     @application = Application.new
   end
+  
+  def create
+    @application = Application.new(application_params)
+    @application.user = current_user
+    
+    if @application.save
+      flash[:notice] = "Your application has been added to your account."
+      redirect_to applications_path
+    else
+      flash[:error] = "There was an error saving your application. Please try again."
+      redirect_to new_application_path
+    end
+  end
+  
+  
+  private
+  
+  def application_params
+    params.require(:application).permit(:url)
 end
